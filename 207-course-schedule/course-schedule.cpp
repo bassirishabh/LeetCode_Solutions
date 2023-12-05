04f5cf2cb2e72c44a794cbbfb181051fcd453c14
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-05 13:04:14 -0600

    Time: 12 ms (94.23%) | Memory: 14.3 MB (53.67%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> numdegrees(numCourses);
+        vector<vector<int>> adj(numCourses);
+
+        for(auto p:prerequisites){
+            adj[p[1]].push_back(p[0]);
+            numdegrees[p[0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(numdegrees[i]==0) q.push(i);
+        }
+        int visit=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            visit++;
+            for(auto adj:adj[node]){
+                numdegrees[adj]--;
+                if(numdegrees[adj]==0) q.push(adj);
+            }
+        }
+        return visit==numCourses;
+    }
+};
