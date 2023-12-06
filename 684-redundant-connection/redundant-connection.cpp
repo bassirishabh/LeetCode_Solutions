e6404a2d6e975f47341155eb733909bf6d79de5b
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-05 19:13:27 -0600

    Time: 9 ms (34.65%) | Memory: 9.3 MB (47.18%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,43 +1,43 @@
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
-#include <stdexcept>
-
-using namespace std;
-
 class Solution {
-private:
-    unordered_set<int> seen;
-    const int MAX_EDGE_VAL = 1000;
-
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<vector<int>> graph(MAX_EDGE_VAL + 1, vector<int>());
-        
-        for (int i = 0; i <= MAX_EDGE_VAL; i++) {
-            graph[i] = vector<int>();
-        }
-
-        for (const auto& edge : edges) {
-            seen.clear();
-            if (!graph[edge[0]].empty() && !graph[edge[1]].empty() &&
-                    dfs(graph, edge[0], edge[1])) {
-                return edge;
-            }
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
-        }
-        return {};
+    int find_parent(int node,vector<int>& parent){
+        if(node==parent[node]) return node;
+        return parent[node]=find_parent(parent[node],parent);
     }
+    void union_find(int u,int v,vector<int>& parent,vector<int>& rank ){
+        u=find_parent(u,parent);
+        v=find_parent(v,parent);
 
-    bool dfs(const vector<vector<int>>& graph, int source, int target) {
-        if (seen.find(source) == seen.end()) {
-            seen.insert(source);
-            if (source == target) return true;
-            for (int nei : graph[source]) {
-                if (dfs(graph, nei, target)) return true;
+        if(rank[u]>rank[v]){
+            parent[v]=u;
+        }else if(rank[v]>rank[u]){
+            parent[u]=v;
+        }else{
+            parent[v]=u;
+            rank[u]+=1;
+        }
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        vector<int> rank(n+1,0);
+        vector<int> parent(n+1,0);
+
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+        }
+        vector<int> ans(2,0);
+
+        for(auto e:edges){
+            int u=e[0];
+            int v=e[1];
+
+            if(find_parent(u,parent)!=find_parent(v,parent)){
+                union_find(u,v,parent,rank);
+            }else if(find_parent(u,parent)==find_parent(v,parent)){
+                ans[0]=u;
+                ans[1]=v;
             }
         }
-        return false;
+        return ans;
     }
 };
