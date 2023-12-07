926f305dea03926959ebf606944edbb6e544900e
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-06 20:12:34 -0600

    Time: 381 ms (89.52%) | Memory: 146.7 MB (74.93%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -8,6 +8,7 @@ public:
         if(triplets[i][0]==target[0] && triplets[i][1]<=target[1] && triplets[i][2]<=target[2]) one=true;
         if(triplets[i][1]==target[1] && triplets[i][2]<=target[2] && triplets[i][0]<=target[0]) two=true;
         if(triplets[i][2]==target[2] && triplets[i][1]<=target[1] && triplets[i][0]<=target[0]) three=true;
+        if(one && two && three) break;
       }
         return one && two && three;
     }
