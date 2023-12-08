9e3f5a10f8abcddbfdab6082e6ac3a825b6b83ef
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-07 21:08:50 -0600

    Time: 23 ms (58.55%) | Memory: 22.8 MB (30.73%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-
-    int calculate(vector<int>& nums, int target,int i,long long total){
-      if(i==nums.size()){
-        if(total==target) return 1;
-        else return 0;
-      }
-      return calculate(nums,target,i+1,total+nums[i])+calculate(nums,target,i+1,total-nums[i]);
-
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
+        int total = 0;
+        int n=nums.size();
+        for (int i = 0; i < n; i++) {
+            total += nums[i];
+        }
+        vector<vector<int>> dp(nums.size(), vector<int>(2*total + 1, 0));
+        dp[0][nums[0]+total]=1;
+        dp[0][-nums[0]+total]+=1;
 
-      
-      return calculate(nums,target,0,0);
-        
+        for(int i=1;i<nums.size();i++){
+          for(int sum=-total;sum<=total;sum++){
+            if(dp[i-1][sum+total]>0){
+              dp[i][sum+nums[i]+total]+=dp[i-1][sum+total];
+              dp[i][sum-nums[i]+total]+=dp[i-1][sum+total];
+            }
+          }
+        }
+        return abs(target)>total?0:dp[nums.size()-1][target+total];
     }
 };
