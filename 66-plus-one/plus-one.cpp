c29508e4c9af34d45671543cd8014ec7855ac88f
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-08 12:39:47 -0600

    Time: 4 ms (29.93%) | Memory: 9 MB (64.52%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size()-1;
+        for(int i=n;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }else{
+                digits[i]+=1;
+                return digits;
+            }
+        }
+        if(digits[0]==0) {
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
