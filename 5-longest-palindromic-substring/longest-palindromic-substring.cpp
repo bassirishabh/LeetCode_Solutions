0c09c85944188eaf3a33584b8e4bc95ad91b2c31
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-17 20:25:37 -0600

    Time: 95 ms (44.63%) | Memory: 171.7 MB (17.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
+    string expand(string s, int i,int j){
+        while(i>=0 && j<s.size() && s[i]==s[j]){
+            i--;
+            j++;
+        }
+        return s.substr(i+1,j-i-1);
+    }
     string longestPalindrome(string s) {
-        vector<vector<int>> dp(s.size(),vector<int>(s.size(),0));
-        string result="";
-        int maxlen=0;
-        for(int diff=0;diff<s.size();diff++){
-            for(int i=0,j=i+diff;j<s.size();i++,j++){
-                if(i==j) dp[i][j]=1;
-                else if(diff==1) dp[i][j]=(s[i]==s[j])?2:0;
-                else{
-                    if(s[i]==s[j] && dp[i+1][j-1]){
-                        dp[i][j]=dp[i+1][j-1]+2;
-                    }
-                }
-                if(dp[i][j]){
-                    if(maxlen<j-i+1){
-                        maxlen=j-i+1;
-                        result=s.substr(i,maxlen);
-                    }
-                }
+        if(s.size()<=1) return s;
+        string maxstr="";
+        for(int i=0;i<s.size()-1;i++){
+            string odd=expand(s,i,i);
+
+            string even=expand(s,i,i+1);
+            cout<<i<<" "<<odd<<" "<<even;
+            if(odd.size()>maxstr.size()){
+                maxstr=odd;
+            }
+            if(even.size()>maxstr.size()){
+                maxstr=even;
             }
         }
-        return result;
+        return maxstr;
     }
 };
