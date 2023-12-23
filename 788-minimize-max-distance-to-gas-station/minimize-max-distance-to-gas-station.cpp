b689b944c3350adb07b4e8c3223bf6794a7ebdc4
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-23 13:20:53 -0600

    Time: 57 ms (8.24%) | Memory: 13.7 MB (23.53%) - LeetSync

---
+++ 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int numberOfGasStationsRequired(long double dist, vector<int> &arr) {
+    int n = arr.size(); // size of the array
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
+        if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+            numberInBetween--;
+        }
+        cnt += numberInBetween;
+    }
+    return cnt;
+}
+
+    double minmaxGasDist(vector<int>& stations, int k) {
+         int n = stations.size(); // size of the array
+    long double low = 0;
+    long double high = 0;
+
+    //Find the maximum distance:
+    for (int i = 0; i < n - 1; i++) {
+        high = max(high, (long double)(stations[i + 1] - stations[i]));
+    }
+
+    //Apply Binary search:
+    long double diff = 1e-6 ;
+    while (high - low > diff) {
+        long double mid = (low + high) / (2.0);
+        int cnt = numberOfGasStationsRequired(mid, stations);
+        if (cnt > k) {
+            low = mid;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return high;
+    }
+};
