99b3ab17b0e9cd6ba1c19f24eb34817217c01f15
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-30 20:50:25 +0530

    Time: 210 ms (8.35%) | Memory: 75.6 MB (94.50%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -5,21 +5,20 @@ public:
         st.push(-1);
 
         int maxarea=0;
-
         for(int i=0;i<heights.size();i++){
             while(st.top()!=-1 && heights[st.top()]>=heights[i]){
-                int curr=heights[st.top()];
+                int cur=heights[st.top()];
                 st.pop();
                 int width=i-st.top()-1;
-                maxarea=max(maxarea,curr*width);
+                maxarea=max(maxarea,cur*width);
             }
             st.push(i);
         }
         while(st.top()!=-1){
-            int curr=heights[st.top()];
+            int cur=heights[st.top()];
             st.pop();
             int width=heights.size()-st.top()-1;
-            maxarea=max(maxarea,width*curr);
+            maxarea=max(maxarea,cur*width);
         }
         return maxarea;
     }
