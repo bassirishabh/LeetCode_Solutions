5d51dc0b56cf83a573620d754a7f16bd438396a5
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-31 12:46:58 +0530

    Time: 1022 ms (22.37%) | Memory: 542.4 MB (74.27%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,48 @@
+struct TrieNode{
+    unordered_map<char,TrieNode*> children;
+    bool endofword=false;
+};
+
+class WordDictionary {
+public:
+    TrieNode* root=new TrieNode();
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        TrieNode* node=root;
+        for(auto c:word){
+            if(node->children.find(c)==node->children.end()){
+                node->children[c]=new TrieNode();
+            }
+            node=node->children[c];
+        }
+        node->endofword=true;
+    }
+    
+    bool searchhelper(string word,TrieNode* node,int index){
+        for(int i=index;i<word.size();i++){
+            if(word[i]=='.'){
+                for(auto p:node->children){
+                    if(searchhelper(word,p.second,i+1)) return true;
+                }
+                return false;
+            }else{
+                if(node->children.find(word[i])==node->children.end()) return false;
+                node=node->children[word[i]];
+            }
+        }
+        return node->endofword;
+    }
+    bool search(string word) {
+        return searchhelper(word,root,0);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
