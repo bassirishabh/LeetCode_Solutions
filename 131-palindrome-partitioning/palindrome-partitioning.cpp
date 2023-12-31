8652722fd9094928dd3301730bf2f9c1ae83b644
Rishabh Bassi <bassirishabh@gmail.com>
2023-12-31 14:40:24 +0530

    Time: 375 ms (5.06%) | Memory: 171.7 MB (8.35%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     vector<vector<string>> result;
 
-    void backtrack(string s, vector<string> curr,int index){
-        if(index>=s.size()){
-            result.push_back(curr);
+    void backtrack(string s, int index, vector<string> temp){
+        if(index==s.size()){
+            result.push_back(temp);
             return;
         }
         for(int i=index;i<s.size();i++){
             if(isvalid(s,index,i)){
-                curr.push_back(s.substr(index,i-index+1));
-                backtrack(s,curr,i+1);
-                curr.pop_back();
+                temp.push_back(s.substr(index,i-index+1));
+                backtrack(s,i+1,temp);
+                temp.pop_back();
             }
-            
         }
     }
-    bool isvalid(string &s, int low, int high) {
-        while (low < high) {
-            if (s[low++] != s[high--]) return false;
+    bool isvalid(string s, int left,int right){
+        while(left<right){
+            if(s[left]!=s[right]) return false;
+            left++;
+            right--;
         }
         return true;
     }
     vector<vector<string>> partition(string s) {
-        backtrack(s,{},0);
+        backtrack(s,0,{});
         return result;
     }
 };
