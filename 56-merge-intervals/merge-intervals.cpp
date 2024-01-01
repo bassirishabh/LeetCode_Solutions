446e75df8a484726e8df2d88c3cc0d2c47e618b0
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-01 19:34:25 +0530

    Time: 26 ms (89.91%) | Memory: 20.3 MB (26.06%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,12 +4,12 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> result;
 
-        for(auto it:intervals){
-          if(result.empty() || result.back()[1]<it[0]){
-            result.push_back(it);
-          }else{
-            result.back()[1]=max(result.back()[1],it[1]);
-          }
+        for(auto i:intervals){
+            if(result.empty() || result.back()[1]<i[0]){
+                result.push_back(i);
+            }else{
+                result.back()[1]=max(result.back()[1],i[1]);
+            }
         }
         return result;
     }
