c0a89aa124a12ebf4cf9cd6ef230e6acb75adf96
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-04 21:56:49 +0530

    Time: 117 ms (54.12%) | Memory: 128.4 MB (97.41%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -3,15 +3,16 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int totalgain=0;
         int curgain=0;
-        int answer=0;
+        int ans=0;
+
         for(int i=0;i<gas.size();i++){
-          totalgain+=(gas[i]-cost[i]);
-          curgain+=(gas[i]-cost[i]);
-          if(curgain<0){
-            curgain=0;
-            answer=i+1;
-          }
+            totalgain+=(gas[i]-cost[i]);
+            curgain+=(gas[i]-cost[i]);
+            if(curgain<0){
+                curgain=0;
+                ans=i+1;
+            }
         }
-        return (totalgain>=0)?answer:-1;
+        return (totalgain>=0)?ans:-1;
     }
 };
