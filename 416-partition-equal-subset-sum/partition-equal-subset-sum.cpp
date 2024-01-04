edda62b28bbd4b11cc438f26c2588ae31abaefd9
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-04 12:46:06 +0530

    Time: 575 ms (16.07%) | Memory: 13.7 MB (62.11%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int totalsum=0;
-        for(int num:nums) totalsum+=num;
-
-        if(totalsum%2!=0) return false;
-        int subsetsum=totalsum/2;
+    bool subset(vector<int> nums, int targetsum){
         int n=nums.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(subsetsum+1,false));
+        vector<vector<bool>> dp(n,vector<bool>(targetsum+1,false));
 
-        dp[0][0]=true;
-        for(int i=1;i<=n;i++){
-          int curr=nums[i-1];
-          for(int j=0;j<=subsetsum;j++){
-            if(j<curr) dp[i][j]=dp[i-1][j];
-            else dp[i][j]=dp[i-1][j] || dp[i-1][j-curr];
-          }
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        // if (nums[0] <= targetsum)
+        //     dp[0][nums[0]] = true;
+
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=targetsum;target++){
+                bool nottake=dp[i-1][target];
+                bool take=false;
+                if(nums[i]<=target) take=dp[i-1][target-nums[i]];
+                dp[i][target]=take || nottake;
+            }
         }
-        return dp[n][subsetsum];
-
+        return dp[n-1][targetsum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int targetsum=0;
+        for(auto i:nums){
+            targetsum+=i;
+        }
+        if (targetsum % 2 == 1) return false;
+        return subset(nums,targetsum/2);
     }
 };
