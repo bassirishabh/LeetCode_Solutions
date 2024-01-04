58548fbd7fef0695d0df908f99452934ec73298f
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-04 12:10:01 +0530

    Time: 318 ms (28.13%) | Memory: 129.4 MB (52.95%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,30 +1,30 @@
 class TimeMap {
 public:
-    unordered_map<string,vector<pair<int,string>>> umap;
+    unordered_map<string,vector<pair<int,string>>> mp;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        umap[key].push_back({timestamp,value});
+        mp[key].push_back({timestamp,value});
     }
     
     string get(string key, int timestamp) {
-        if(umap.find(key)==umap.end()) return "";
-        if(timestamp<umap[key][0].first) return "";
-
+        if(mp.find(key)==mp.end()) return "";
+        if(mp[key][0].first>timestamp) return "";
         int left=0;
-        int right=umap[key].size()-1;
+        int right=mp[key].size()-1;
+        string ans="";
         while(left<=right){
             int mid=(left+right)/2;
-            if(umap[key][mid].first<=timestamp){
+            if(mp[key][mid].first<=timestamp){
+                ans=mp[key][mid].second;
                 left=mid+1;
             }else{
                 right=mid-1;
             }
         }
-        if(right==-1) return "";
-        return umap[key][right].second;
+        return ans;
     }
 };
 
