8dfab3a294bcd6581b9dd134a2e3723a3a273d66
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-04 18:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (47.99%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -4,17 +4,18 @@ public:
         unordered_map<char,int> umap;
 
         for(int i=0;i<s.size();i++){
-          umap[s[i]]=i;
+            umap[s[i]]=i;
         }
         vector<int> result;
-        int size=0,end=0;
+        int size=0;
+        int end=0;
         for(int i=0;i<s.size();i++){
-          size++;
-          end=max(end,umap[s[i]]);
-          if(i==end){
-            result.push_back(size);
-            size=0;
-          }
+            size++;
+            end=max(end,umap[s[i]]);
+            if(i==end){
+                result.push_back(size);
+                size=0;
+            }
         }
         return result;
     }
