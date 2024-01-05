899ade5a6d2b1c8d40da6debc3f79f07d25c9b51
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-05 12:26:19 +0530

    Time: 10 ms (28.59%) | Memory: 9.4 MB (46.32%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,43 +1,49 @@
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+};
 class Solution {
 public:
-    int find_parent(int node,vector<int>& parent){
-        if(node==parent[node]) return node;
-        return parent[node]=find_parent(parent[node],parent);
-    }
-    void union_find(int u,int v,vector<int>& parent,vector<int>& rank ){
-        u=find_parent(u,parent);
-        v=find_parent(v,parent);
-
-        if(rank[u]>rank[v]){
-            parent[v]=u;
-        }else if(rank[v]>rank[u]){
-            parent[u]=v;
-        }else{
-            parent[v]=u;
-            rank[u]+=1;
-        }
-    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        vector<int> rank(n+1,0);
-        vector<int> parent(n+1,0);
-
-        for(int i=1;i<=n;i++){
-            parent[i]=i;
-        }
-        vector<int> ans(2,0);
-
-        for(auto e:edges){
-            int u=e[0];
-            int v=e[1];
-
-            if(find_parent(u,parent)!=find_parent(v,parent)){
-                union_find(u,v,parent,rank);
-            }else if(find_parent(u,parent)==find_parent(v,parent)){
-                ans[0]=u;
-                ans[1]=v;
+        DisjointSet d(n);
+        vector<vector<int>> adj;
+        for(auto it:edges){
+            if(d.findUPar(it[0])==d.findUPar(it[1])){
+                return it;
+            }else{
+                d.unionByRank(it[0],it[1]);
             }
         }
-        return ans;
+        return {};
     }
 };
