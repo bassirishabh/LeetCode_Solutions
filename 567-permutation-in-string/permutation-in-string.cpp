2edc8893955e655df0533f65852da816f4ff9bb2
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-10 14:20:51 +0530

    Time: 35 ms (20.82%) | Memory: 30.2 MB (15.81%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -8,14 +8,15 @@ public:
     }
     bool checkInclusion(string s1, string s2) {
         if(s1.size()>s2.size()) return false;
+
         vector<int> freqmap1(26,0);
         vector<int> freqmap2(26,0);
 
-        for(auto i:s1){
-            freqmap1[i-'a']++;
-        }
+        for(auto i:s1) freqmap1[i-'a']++;
+
         int i=0;
         int j=0;
+
         while(j<s2.size()){
             freqmap2[s2[j]-'a']++;
             if(j-i+1==s1.size()){
