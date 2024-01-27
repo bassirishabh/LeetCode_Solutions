a39daf278808b26142a08a2b7bdb704a7db27c73
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-27 16:53:22 -0600

    Time: 41 ms (87.73%) | Memory: 50.8 MB (18.55%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -3,9 +3,9 @@ public:
     bool canJump(vector<int>& nums) {
         int goal=nums.size()-1;
 
-        for(int i=nums.size()-2;i>=0;i--){
-          if(i+nums[i]>=goal) goal=i;
+        for(int i=goal;i>=0;i--){
+            if(i+nums[i]>=goal) goal=i;
         }
-        return goal==0;
+        return (goal==0);
     }
 };
