585ae27981646a7d7bad5f8f44e946f8819df115
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-27 17:20:39 -0600

    Time: 6 ms (95.54%) | Memory: 19 MB (11.05%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int right=0;
         int left=0;
-        int maxfar=0;
+        int right=0;
         int result=0;
+        int maxsofar=0;
+
         while(right<nums.size()-1){
-          for(int i=left;i<=right;i++){
-            maxfar=max(maxfar,i+nums[i]);
-          }
-          left=right;
-          right=maxfar;
-          result+=1;
+            for(int i=left;i<=right;i++){
+                maxsofar=max(maxsofar,i+nums[i]);
+            }
+            result+=1;
+            left=right;
+            right=maxsofar;
         }
         return result;
     }
