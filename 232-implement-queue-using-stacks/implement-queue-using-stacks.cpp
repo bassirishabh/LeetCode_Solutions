8dda2bfa3650acfe12ce8f4e034841ff89bfd706
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-30 07:59:13 -0600

    Time: 0 ms (100.00%) | Memory: 8.5 MB (5.85%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -6,30 +6,30 @@ public:
     }
     
     void push(int x) {
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
-        s2.push(x);
         while(!s1.empty()){
             s2.push(s1.top());
             s1.pop();
         }
-
+        s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        
     }
     
     int pop() {
-        int val=s2.top();
-        s2.pop();
-        return val;
+        int tp=s1.top();
+        s1.pop();
+        return tp;
     }
     
     int peek() {
-        return s2.top();
+        return s1.top();
     }
     
     bool empty() {
-        return s2.empty();
+        return s1.empty();
     }
 };
 
