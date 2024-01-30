2ad9c17d0e0ade5775d818f717d445722509803c
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-30 07:39:19 -0600

    Time: 4 ms (8.36%) | Memory: 8.1 MB (5.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         unordered_map<char,char> umap;
+        umap['}']='{';
         umap[')']='(';
         umap[']']='[';
-        umap['}']='{';
-
         stack<char> st;
-
         for(int i=0;i<s.size();i++){
             if(umap.find(s[i])!=umap.end()){
-                if(st.empty() || st.top()!=umap[s[i]]){
-                     
-                    return false;
-                }
+                if(st.empty()) return false;
+                if(st.top()!=umap[s[i]]) return false;
                 st.pop();
-               
-            }else st.push(s[i]);
-
+            }else{
+                st.push(s[i]);
+            }
         }
         return st.empty();
     }
