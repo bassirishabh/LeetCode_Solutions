f324ed87a14b244f2480886e01c898127470a7da
Rishabh Bassi <bassirishabh@gmail.com>
2024-01-30 22:07:47 -0600

    Time: 73 ms (91.42%) | Memory: 28.9 MB (45.73%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,20 +5,20 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=nums.size()-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
+            int left=i+1;
+            int right=nums.size()-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
                 if(sum==0){
-                    vector<int> temp={nums[i],nums[j],nums[k]};
+                    vector<int> temp={nums[i],nums[left],nums[right]};
                     result.push_back(temp);
-                    while(j<k && nums[j]==temp[1]) j++;
-                    while(j<k && nums[k]==temp[2]) k--;
-
-                }else if(sum<0){
-                    j++;
+                    while(left<right && nums[left]==temp[1]) left++;
+                    while(left<right && nums[right]==temp[2]) right--;
+                }
+                else if(sum<0){
+                    left++;
                 }else{
-                    k--;
+                    right--;
                 }
             }
         }
