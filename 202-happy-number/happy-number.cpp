34ad2a180897da3c0d6bafd1a3de98a7ef73cab7
Rishabh Bassi <bassirishabh@gmail.com>
2024-02-02 10:44:46 -0600

    Time: 0 ms (100.00%) | Memory: 7.1 MB (17.03%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int getnext(int n){
         int result=0;
+
         while(n){
             result+=(n%10)*(n%10);
             n=n/10;
@@ -11,12 +12,11 @@ public:
     bool isHappy(int n) {
         int slow=n;
         int fast=getnext(n);
+
         while(fast!=1 && slow!=fast){
             slow=getnext(slow);
             fast=getnext(getnext(fast));
-            
         }
         return (fast==1)?true:false;
-
     }
 };
