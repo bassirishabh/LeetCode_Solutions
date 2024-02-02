615ab6c650ff15c2fbf07565eaa69b188e40107e
Rishabh Bassi <bassirishabh@gmail.com>
2024-02-02 11:46:34 -0600

    Time: 6 ms (63.88%) | Memory: 7.9 MB (29.47%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<char> sM(128,0);
-        vector<char> tM(128,0);
+        vector<int> charS(128,0);
+        vector<int> charT(128,0);
         if(s.size()!=t.size()) return false;
-
         for(int i=0;i<s.size();i++){
-            if(sM[s[i]]!=tM[t[i]]) return false;
-            
-            sM[s[i]]=i+1;
-            tM[t[i]]=i+1;
+            if(charS[s[i]]!=charT[t[i]]) return false;
+            charS[s[i]]=i+1;
+            charT[t[i]]=i+1;
         }
         return true;
     }
