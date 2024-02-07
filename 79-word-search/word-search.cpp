7d30bc3d8a0f22e93772df132e2f49c10e9f90f7
Rishabh Bassi <bassirishabh@gmail.com>
2024-02-07 09:39:12 -0600

    Time: 995 ms (26.54%) | Memory: 11.6 MB (23.03%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int rows;
     int cols;
-    
-    bool backtrack(int i,int j,vector<vector<char>>& board, string word,int index){
+
+    bool backtrack(vector<vector<char>>& board, string word,int i,int j,int index){
         if(index==word.size()) return true;
+
         if(i<0 || i>=rows || j<0 || j>=cols || board[i][j]!=word[index]) return false;
-        
         char ch=board[i][j];
         board[i][j]='#';
-
-        bool result=backtrack(i+1,j,board,word,index+1) || backtrack(i-1,j,board,word,index+1)|| backtrack(i,j+1,board,word,index+1)|| backtrack(i,j-1,board,word,index+1);
+        bool result=backtrack(board,word,i+1,j,index+1)||backtrack(board,word,i-1,j,index+1)||backtrack(board,word,i,j+1,index+1)||backtrack(board,word,i,j-1,index+1);
         board[i][j]=ch;
         return result;
-
-
     }
     bool exist(vector<vector<char>>& board, string word) {
         rows=board.size();
@@ -22,7 +19,7 @@ public:
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(backtrack(i,j,board,word,0)) return true;
+                if(backtrack(board,word,i,j,0)) return true;
             }
         }
         return false;
